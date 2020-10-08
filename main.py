c611b8a36bef2fd5c7b6dc6884a45be72ff45a01
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2020-10-08 15:37:00 +0200

    add support input.csv

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-# objectos
-W = [  2,  5,  1,  6,  2]
-U = [ 28, 33,  5, 12, 20]
+import numpy as np
+import csv
 
-N = len(W)  # numero de objetos
-M = 5       # peso maximo
+# objectos
+# W = [  2,  5,  1,  6,  2]
+# U = [ 28, 33,  5, 12, 20]
+# N = len(W)  # numero de objetos
+# M = 5       # peso maximo
+
+
+W = 0      # peso
+U = 0      # utilidad
+N = 0      # numero de objetos
+M = 0      # peso maximo
+
+with open('input.csv', 'r') as f:
+    data = np.array(list(csv.reader(f, delimiter=',')), dtype=int)
+
+    N = data[0][0]
+    M = data[0][1]
+
+    data = data[1:]
+
+    U = data[:, 0]
+    W = data[:, 1]
+
 
 # buffer (N+1) x (M+1)
 R = [[0 for m in range(M+1)] for i in range(N+1)]
-
-DEBUG = True
 
 """
 R[x] <- x es el numero de objetos que ha introducido en orden
@@ -36,20 +54,10 @@ ver otras alternativas como R[N][5] para todos los objetos con peso max 5
 
 for i in range(1, N+1):
     for m in range(1, M+1):
-        if DEBUG:
-            print('[', i, m, ']',
-                    W[i-1], R[i-1][m-W[i-1]], m-W[i-1],
-                    U[i-1] + R[i-1][m-W[i-1]], R[i-1][m],
-                    W[i-1] <= m, U[i-1] + R[i-1][m-W[i-1]] > R[i-1][m])
 
         if W[i-1] <= m and U[i-1] + R[i-1][m-W[i-1]] > R[i-1][m]:
             R[i][m] = U[i-1] + R[i-1][m-W[i-1]]
         else:
             R[i][m] = R[i-1][m]
-
-if DEBUG:
-    for t in R:
-        print(t)
-    print()
 
 print(R[N][M])
